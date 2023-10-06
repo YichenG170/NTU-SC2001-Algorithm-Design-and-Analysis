a726bd9fbf7ad33d9b567d1d0219c70e2a17cefd
YM <110095482+YichenG170@users.noreply.github.com>
2023-10-06 22:31:04 +0800

    Update DijkstraMinHeap.java

--- L2/DijkstraMinHeap.java
+++ L2/DijkstraMinHeap.java
@@ -1,53 +1,50 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class DijkstraMinHeap {
-	public static void dijkstra(int V, ArrayList<ArrayList<Node>> graph, int source) {
-		int[] dist = new int[V];
+    public static void findShortestPaths(int numberOfVertices, ArrayList<ArrayList<Node>> graph, int source) {
+        int[] distances = new int[numberOfVertices];
+        for (int i = 0; i < numberOfVertices; i++) {
+            distances[i] = Integer.MAX_VALUE;
+        }
 
-		for (int i = 0; i < V; i++)
-			dist[i] = Integer.MAX_VALUE;
+        distances[source] = 0;
 
-		dist[source] = 0;
+        PriorityQueue<Node> minHeap = new PriorityQueue<>((node1, node2) -> node1.getCost() - node2.getCost());
+        minHeap.add(new Node(source, 0));
 
-		PriorityQueue<Node> minHeap = new PriorityQueue<>((v1, v2) -> v1.getCost() - v2.getCost());
-		minHeap.add(new Node(source, 0));
+        while (!minHeap.isEmpty()) {
+            Node currentNode = minHeap.poll();
 
-		while (minHeap.size() > 0) {
-			Node current = minHeap.poll();
+            for (Node neighbor : graph.get(currentNode.getVertex())) {
+                int potentialDistance = distances[currentNode.getVertex()] + neighbor.getCost();
+                if (potentialDistance < distances[neighbor.getVertex()]) {
+                    distances[neighbor.getVertex()] = potentialDistance;
+                    minHeap.add(new Node(neighbor.getVertex(), potentialDistance));
+                }
+            }
+        }
 
-			for (Node node : graph.get(current.getVertex())) {
-				if (dist[current.getVertex()] + node.getCost() < dist[node.getVertex()]) {
-					dist[node.getVertex()] = dist[current.getVertex()] + node.getCost();
-					minHeap.add(new Node(node.getVertex(), dist[node.getVertex()]));
-				}
-			}
-		}
+        for (int vertex = 0; vertex < numberOfVertices; vertex++) {
+            System.out.println("Vertex: " + vertex + " Minimum Distance from Source: " + distances[vertex]);
+        }
+    }
 
-		for (int j = 0; j < V; j++)
-			System.out.println("Vertex: " + j + " Minimum Distance from Source: " + dist[j]);
+    public static void main(String[] args) {
+        int numberOfVertices = 100;
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
 
-	}
+        int[][] adjacencyMatrix = RandomArray.randomSquareArray(numberOfVertices, -5, 10);
+        RandomArray.print2DArray(adjacencyMatrix);
 
-	public static void main(String[] args) {
-		int V = 100;
-		ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-		int[][] intGraph;
+        int source = 0;
 
-		// Should have negative numbers at min so that some nodes are not connected
-		// If min is positive then all nodes are connected to each other le
-		// Unless that is what you want la
-		intGraph = RandomArray.random2DSquareArray(V, -5, 10);
+        RandomArray.to2DGraph(adjacencyMatrix, graph);
 
-		RandomArray.print2DArray(intGraph);
-
-		int source = 0;
-
-		RandomArray.to2DGraph(intGraph, graph);
-
-		long startTime = System.nanoTime();
-		dijkstra(V, graph, source);
-		long endTime = System.nanoTime();
-		long duration = (endTime - startTime) / 1000000;
-		System.out.println("Duration: " + duration + "ms");
-	}
+        long startTime = System.nanoTime();
+        findShortestPaths(numberOfVertices, graph, source);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000; // in milliseconds
+        System.out.println("Duration: " + duration + "ms");
+    }
 }
